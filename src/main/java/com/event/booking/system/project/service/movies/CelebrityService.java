398261589abc0fa15da0f653fecc3c9618b11a20
krish263 <krishnabadde115@gmail.com>
2024-07-07 22:54:20 +0530

    add something in service class

--- src/main/java/com/event/booking/system/project/service/movies/CelebrityService.java
+++ src/main/java/com/event/booking/system/project/service/movies/CelebrityService.java
@@ -3,6 +3,7 @@ package com.event.booking.system.project.service.movies;
 import com.event.booking.system.project.entity.movies.Celebrity;
 import jakarta.transaction.Transactional;
 
+//added in master branch
 @Transactional
 public interface CelebrityService {
     public void addCelebrity(Celebrity celebrity);
